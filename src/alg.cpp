b2d8a369a49187fb3d7fa8bb18d6df2fb31078de
Axeber0 <axeber.yt@gmail.com>
2025-02-13 14:23:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,27 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {
-
+int gcd(int x, int y) {
+	int nod = 0;
+	while (!nod) {
+		if (x > y) {
+			for (int i = y; i; i--) {
+				if (((y % i) == 0) && ((x % i) == 0)) {
+					nod = i;
+					break;
+				}
+			}
+		}
+		if (x < y) {
+			for (int i = x; i; i--) {
+				if (((y % i) == 0) && ((x % i) == 0)) {
+					nod = i;
+					break;
+				}
+			}
+		}
+		if (x == y) {
+			nod = x;
+		}
+	}
+	return nod;
 }
